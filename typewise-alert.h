eeaeac7cd759acffb7bdc691bc2561503b42fb9e
deepasn08 <deepareddy997@gmail.com>
2022-08-08 23:44:46 +0530

    changes v1

--- typewise-alert.h
+++ typewise-alert.h
@@ -1,4 +1,16 @@
 #pragma once
+#include <stdio.h>
+
+#define COOLING_LOWLIMIT 0
+#define PASSIVECOOLING_UPPERLIMIT  35
+#define ACTIVECOOLING_UPPERLIMIT 45
+#define MED_ACTIVECOOLING_UPPERLIMIT 40
+
+#define header 0xfeed
+#define recepient "a.b@c.com"
+
+#define LOW_TEMPWARN "Hi, the temperature is too low"
+#define HIGH_TEMPWARN "Hi, the temperature is too high"
 
 typedef enum {
   PASSIVE_COOLING,
